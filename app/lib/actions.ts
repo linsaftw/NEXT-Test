23450ac22d7076d8edefed07bc2aa7e7072c2627
linsaftw <linsaftw@users.noreply.github.com>
2024-03-04 03:36:24 -0300

    chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,10 +21,14 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
+    try {
     await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+    } catch (error) {
+        return {"message": "Error creating invoice"};
+    }
  
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -44,17 +48,25 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
+  try {
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+} catch (error) {
+    return {"message": "Error updating invoice"};
+}
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+    try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
+} catch (error) {
+    return {"message": "Error deleting invoice"};
+}
     revalidatePath('/dashboard/invoices');
   }
